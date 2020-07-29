d948cc98e5eb6bf15545a730d658907a4042bf1a
Eduardo Sanz García <esanzgar@gmail.com>
2020-07-29 19:03:01 +0200

    Updates

---
+++ precache-manifest.34b6eb721210e4673adcfef4714362d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e456ab6f43bc0aa98f80455ade73a9ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ed6e5842ba1bff32f4e",
+    "url": "/static/css/main.3dddd02e.chunk.css"
+  },
+  {
+    "revision": "43461986aa1b0957525f",
+    "url": "/static/js/2.74f730e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74f730e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed6e5842ba1bff32f4e",
+    "url": "/static/js/main.118a7a4b.chunk.js"
+  },
+  {
+    "revision": "c8f7fa412379c9adfb38",
+    "url": "/static/js/runtime-main.2dc48fde.js"
+  }
+]);
